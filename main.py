30e93041831d58ad2cece1ace14381a0640406ce
André Carvalho <alac1984@gmail.com>
2022-06-13 23:01:35 -0300

    Basic title bar finished

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.widget import Widget, ObjectProperty, ListProperty
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.config import Config
+from kivy.core.text import LabelBase
 Config.set('graphics', 'resizable', False)
 Config.write()
 
+LabelBase.register(
+    name="Syncopate",
+    fn_regular="Syncopate-Regular.ttf",
+    fn_bold="Syncopate-Bold.ttf"
+)
 
-kv = Builder.load_file('app.kv')
+
+Builder.load_file('app.kv')
+
+
+class TitleBar(BoxLayout):
+    def focus(*args, **kwargs):
+        print(args)
+
+
+class Root(GridLayout):
+    pass
 
 
 class MainApp(App):
     def build(self):
-        return kv
+        return Root()
 
 
 if __name__ == "__main__":
